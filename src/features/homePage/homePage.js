88efb62202e2e37c19f4fbdd075fd5f707a6d98b
NickolasBenakis <nickolasbenele@gmail.com>
2019-04-12 11:33:25 +0300

    Add homepage & navbar

--- src/features/homePage/homePage.js
+++ src/features/homePage/homePage.js
@@ -1,20 +1,29 @@
 
 
 
-angular.module('homePage', [])
-    .config(['$routeProvider',
-        function config($routeProvider) {
-            $routeProvider.
-                when('/homePage', {
-                    templateUrl: './src/features/homePage/homePage.html',
-                    controller: 'homePageCtrl'
-                })
-        }
-    ]).controller('homePageCtrl', homePageCtrl)
-    .factory('hello', ['serviceDemo']);
+angular.module('homePage', []);
+angular.module('homePage')
+    .controller('homePageCtrl', homePageCtrl)
+    .factory('dataFactory', dataFactory);
 
 
-function homePageCtrl($scope) {
+function homePageCtrl(dataFactory) {
+    var vm = this;
+    vm.title = "Need Music?";
+    
+    dataFactory.getOnlineMovies().then(function (res) {
+        console.dir(res.data.results[0].title);
+        vm.data = res.data.results[0].title;
+    });
+}
 
-    return $scope;
+function dataFactory($http) {
+
+    var url = 'https://api.themoviedb.org/3/movie/now_playing?api_key=bc50218d91157b1ba4f142ef7baaa6a0&page=1';
+    var dataFactory = {};
+
+    dataFactory.getOnlineMovies = function () {
+        return $http.get(url);
+    }
+    return dataFactory;
 }
